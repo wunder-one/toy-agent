0247f2bbcfd7f2eb52c9c15e41af2bc63fb45def
wunder-one <wunderwood@me.com>
2026-03-09 15:07:38 -0700

    cleanup get_files_info

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,16 +1,25 @@
 from functions.get_files_info import get_files_info
 
-print('get_files_info("calculator", ".")')
-print(get_files_info("calculator", "."))
-print('')
+def test():
+    result = get_files_info("calculator", ".")
+    print('get_files_info("calculator", ".")')
+    print(result)
+    print('')
 
-print('get_files_info("calculator", "pkg")')
-print(get_files_info("calculator", "pkg"))
-print('')
+    result = get_files_info("calculator", "pkg")
+    print('get_files_info("calculator", "pkg")')
+    print(result)
+    print('')
 
-print('get_files_info("calculator", "/bin")')
-print(get_files_info("calculator", "/bin"))
-print('')
+    result = get_files_info("calculator", "/bin")
+    print('get_files_info("calculator", "/bin")')
+    print(result)
+    print('')
 
-print('get_files_info("calculator", "../")')
-print(get_files_info("calculator", "../"))
+    result = get_files_info("calculator", "../")
+    print('get_files_info("calculator", "../")')
+    print(result)
+    print('')
+
+if __name__ == "__main__":
+    test()
