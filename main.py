409b4edfabcdffb4e513eb52e6b51dda25d9639f
wunder-one <wunderwood@me.com>
2026-03-08 20:31:46 -0700

    setup main() function

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-print("Loading Environment...")
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if api_key == None:
-    raise RuntimeError("Gemini API key is not set")
+def main():
+    print("Loading Environment...")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
-client = genai.Client(api_key=api_key)
+    client = genai.Client(api_key=api_key)
 
-print("Sending Question...")
-response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-print(f"[Answer]:{response.text}")
+    print("Sending Question...")
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print(f"[Answer]:{response.text}")
+
+if __name__ == "__main__":
+    main()
