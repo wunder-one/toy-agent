daca704ccd592aec9d40466c35c5c2336747796b
wunder-one <wunderwood@me.com>
2026-03-08 21:37:00 -0700

    setup for agent memory

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+from google.genai import types
+
+
 
 def main():
     print("Loading Environment...")
@@ -11,10 +15,16 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    parser = argparse.ArgumentParser(description="Agent Chat")
+    parser.add_argument("user_prompt", type=str, help="Your prompt")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     print("Sending Question...")
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=messages
     )
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
